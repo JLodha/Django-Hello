46c90c17247cfc236d0662633179302320183057
JLodha <iamjanit1@gmail.com>
2023-06-28 16:51:39 +0530

    Model Created and POST API for Contact

--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+
 
 # Create your views here.
-
-
 def index(request):
     context = {
         "variable1": "This is sent variable 123",
@@ -21,4 +23,14 @@ def services(request):
 
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        date = datetime.today()
+        contact = Contact(name=name, email=email,
+                          phone=phone, desc=desc, date=date)
+        contact.save()
+        messages.success(request, "Your message has been sent!")
     return render(request, 'contact.html')
