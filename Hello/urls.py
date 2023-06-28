b078fdacb56e465c4a1d7c324aa9fe7e323576d8
JLodha <iamjanit1@gmail.com>
2023-06-28 14:38:41 +0530

    Basic UI Completed

--- Hello/urls.py
+++ Hello/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Janit IceCreams Admin"
+admin.site.site_title = "Janit IceCreams Admin"
+admin.site.index_title = "Welcome to Janit IceCreams"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls'))
--- home/views.py
+++ home/views.py
@@ -13,12 +13,12 @@ def index(request):
 
 
 def about(request):
-    return HttpResponse("This is about page")
+    return render(request, 'about.html')
 
 
 def services(request):
-    return HttpResponse("This is services page")
+    return render(request, 'services.html')
 
 
 def contact(request):
-    return HttpResponse("This is contact page")
+    return render(request, 'contact.html')
